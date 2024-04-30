fa4d057f88e80ec21126e3d9f827ab97f22d02c8
Herlan <herlanherbas@gmail.com>
2024-04-30 15:59:53 -0400

    feature(the app and gitignore file was updated)

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const { config } = require('dotenv')
+const studentRouter = require('./controller/student.controller')
+config()
+
+app.use(express.json());
+app.use('/student', studentRouter);
 
 
-app.listen(4000);
-console.log('Server port 4000')
+const port = process.env.PORT || 5000
+app.listen(port, ()=>{
+    console.log(`Server listening on ${port}`)
+})
+

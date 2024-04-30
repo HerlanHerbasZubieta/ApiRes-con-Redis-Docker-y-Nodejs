3bfd21ea33eeaff42b9a9b1598fea34a8af26416
Herlan <herlanherbas@gmail.com>
2024-04-30 15:46:35 -0400

    feature(the student model was added)

---
+++ src/model/student.model.js
@@ -0,0 +1,14 @@
+
+class Student {
+    constructor(id, name, lastName, code, email, password){
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.code = code;
+        this.email = email;
+        this.password = password;
+    }
+   
+};
+
+module.exports = Student;

1bd4e729d6c259dedcee60d4d6908c4120f49609
Willy Samuel Paz Colque <willypaz243@gmail.com>
2024-05-01 01:19:18 -0400

    fix docker compose config

--- src/service/initClient.js
+++ src/service/initClient.js
@@ -1,15 +1,14 @@
 const { createClient } = require("redis");
 
 let client = undefined;
-const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const REDIS_HOST = process.env.REDIS_HOST || "localhost";
+const REDIS_PORT = process.env.REDIS_PORT ?? 6379;
+const REDIS_HOST = process.env.REDIS_HOST ?? "redis";
 
 const getClient = async () => {
   if (!client) {
     client = await createClient({
-      host: REDIS_HOST,
-      port: REDIS_PORT,
-      legacyMode: true
+      url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+      legacyMode: true,
     })
       .on("error", (err) => console.log("Redis Client Error", err))
       .connect();

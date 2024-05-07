e3213f1c0e6dc8c863f3ebfa6aab43b95348e383
Herlan <herlanherbas@gmail.com>
2024-05-07 18:01:59 -0400

    initClient and app was updated

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const { config } = require('dotenv')
-const studentRouter = require('./controller/student.controller')
 config()
+const studentRouter = require('./controller/student.controller')
 
 app.use(express.json());
 app.use('/student', studentRouter);
@@ -10,6 +10,6 @@ app.use('/student', studentRouter);
 
 const port = process.env.PORT || 5000
 app.listen(port, ()=>{
-    console.log(`Server listening on ${port}`)
+    console.log(`Server listening on port ${port}`)
 })
 
